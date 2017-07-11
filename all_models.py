0728707dc4df1f1ea5b44045da7baeb7cf55aa2c
mohamedabolfadl <mabolfadl@gmail.com>
2017-07-11 17:24:28 +0200

    Top features included

--- all_models.py
+++ all_models.py
@@ -1,5 +1,6 @@
 
 
+
 # Importing the libraries
 import numpy as np
 import matplotlib.pyplot as plt
@@ -15,9 +16,8 @@ svm_col='orange'
 nn_col = 'r'
 p = 0.11 # Fraction of volume who will churn
 d = 0.2  # Discount
-
 p_a_m = 5 # relationship between dicount provided and probability of accepting it  
-
+N_feat = 10 # Select top N_feat features
 
 
 def acceptProb(d,acceptance_kernel):
@@ -94,25 +94,29 @@ def plotROC(y_test, y_score_l,label,col):
     plt.legend(loc="lower right")
     
 
-plt.figure()
+
+# Indices of optimal features from most significant to least
+slct_inds = [0,12,3,15,21,23,4,26,6,20,2,7,25,16,13,5,19,29,27,10,28,17,1,11,30,24,31,8,18,22,9,34,38,36,33,40,39,32,35,14,37]
 
 # Importing the dataset
 dataset = pd.read_csv('ml_case_training_data_cleaned.csv')
-X = dataset.iloc[:, [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,33,34,35,36,37,38,39,40,41]].values
+#X = dataset.iloc[:, [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,33,34,35,36,37,38,39,40,41]].values
+X = dataset.iloc[:, slct_inds].values
+X = X[:,0:N_feat]
 y = dataset.iloc[:, 32].values
 
 
 # Filling NaNs with their mean
 from sklearn.preprocessing import Imputer
 imputer = Imputer(missing_values = 'NaN', strategy = 'mean', axis = 0)
-imputer = imputer.fit(X[:, 1:32])
-X[:, 1:32] = imputer.transform(X[:, 1:32]) 
+imputer = imputer.fit(X[:, 0:32])
+X[:, 0:32] = imputer.transform(X[:, 0:32]) 
 
 # Splitting the dataset into the Training set and Test set
 from sklearn.cross_validation import train_test_split
 #from sklearn.model_selection import train_test_split
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.1, random_state = 0)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
 # Feature Scaling
 from sklearn.preprocessing import StandardScaler
@@ -120,15 +124,16 @@ sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
 
+plt.figure()
+
+
+
 # Fitting Random Forest Classification to the Training set
 from sklearn.ensemble import RandomForestClassifier
 classifier = RandomForestClassifier(n_estimators = 10, criterion = 'entropy', random_state = 0)
 classifier.fit(X_train, y_train)
-#y_conf_1 = classifier.decision_function(X_test)
 y_conf_2 = classifier.predict_proba(X_test)
-# Predicting the Test set results
 y_pred = classifier.predict(X_test)
-# Making the Confusion Matrix
 from sklearn.metrics import confusion_matrix, accuracy_score
 cm = confusion_matrix(y_test, y_pred)
 print('Random Forest accuracy = ' + str(100.0*accuracy_score(y_test, y_pred)))
@@ -231,9 +236,9 @@ import keras
 from keras.models import Sequential
 from keras.layers import Dense
 classifier = Sequential()
-classifier.add(Dense(units = 35, kernel_initializer = 'uniform', activation = 'relu', input_dim = 41))
-classifier.add(Dense(units = 35, kernel_initializer = 'uniform', activation = 'relu'))
-classifier.add(Dense(units = 25, kernel_initializer = 'uniform', activation = 'relu'))
+classifier.add(Dense(units = int(np.floor(2*(N_feat-1)/3)), kernel_initializer = 'uniform', activation = 'relu', input_dim = N_feat))
+classifier.add(Dense(units = int(np.floor(2*(N_feat-1)/3)), kernel_initializer = 'uniform', activation = 'relu'))
+classifier.add(Dense(units = int(np.floor((N_feat-1)/3)), kernel_initializer = 'uniform', activation = 'relu'))
 classifier.add(Dense(units = 1, kernel_initializer = 'uniform', activation = 'sigmoid'))
 classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
 classifier.fit(X_train, y_train, batch_size = 5, epochs = 100)
@@ -274,6 +279,8 @@ plotRET(y_test, y_conf_LR,lbl='LR',col=lr_col,p=pval,d=dval,plotFlag=True, kern=
 plt.grid()
 plt.ylim([-2,2])
 plt.show()
+
+
 
 
 
